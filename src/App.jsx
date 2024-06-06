4913818e76bf4f704d42ee7d80779b15de48b7ac
Amanda Paul Dull <118822887+pdamanda@users.noreply.github.com>
2024-06-06 14:29:40 +0000

    Estrutura HTML criada

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-      App
+      <div>
+        <h2>Rick Sanches</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+
+
+      </div>
+
     </>
   )
 }
