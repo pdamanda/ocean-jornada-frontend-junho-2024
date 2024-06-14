5cf6b026790d375461347e1c0e9c52eb8d449601
Amanda Paul Dull <118822887+pdamanda@users.noreply.github.com>
2024-06-14 15:08:48 +0000

    Criação do layout da página de visualização

--- src/pages/ViewItemById.jsx
+++ src/pages/ViewItemById.jsx
@@ -1,35 +1,70 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import './ViewItemById.css'
 
 function ViewItemById() {
-  const [item, setItem] = useState([])
+  const [item, setItem] = useState()
 
   const { itemId } = useParams()
-  console.log(itemId)
 
   async function carregarDados() {
-    const apiURL = "https://rickandmortyapi.com/api/character/" + itemId
-    console.log(apiURL)
-    const response = await fetch(apiURL)
+    const apiUrl = 'https://rickandmortyapi.com/api/character/' + itemId
+
+    const response = await fetch(apiUrl)
 
     const body = await response.json()
 
-    const results = body.results
-
     setItem(body)
-
   }
 
   useEffect(function () {
     carregarDados()
   }, [])
+
   if (!item) {
-    return <div> Carregando... </div>
+    return <div>Carregando...</div>
   }
 
   return (
     <>
-      <h1> {item.name} </h1>
+      <div className="view_item">
+        <Link to="/">Voltar para Home</Link>
+
+        <h1>{item.name}</h1>
+
+        <div className="content">
+          <div>
+            <img id="image" src={item.image} />
+          </div>
+
+          <div className="attributes">
+            <div className="attribute">
+              <h2>STATUS</h2>
+              <p>{item.status}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>SPECIES</h2>
+              <p>{item.species}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>GENDER</h2>
+              <p>{item.gender}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>ORIGIN</h2>
+              <p>{item.origin.name}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>LOCATION</h2>
+              <p>{item.location.name}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
