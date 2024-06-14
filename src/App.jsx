0d2e32dd7f60db984142fad5d1e1f488145c8b61
Amanda Paul Dull <118822887+pdamanda@users.noreply.github.com>
2024-06-14 12:41:32 +0000

    Criação da 'Home' e refatoração do 'App'

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import Card from './components/Card'
-import { useEffect } from 'react'
+import Home from './pages/Home'
 
 function App() {
 
-  const [itens, setItens] = useState([])
-
-  async function carregarDados() {
-    const apiURL = "https://rickandmortyapi.com/api/character"
-
-    const response = await fetch(apiURL)
-
-    const body = await response.json()
-
-    const results = body.results
-
-    console.log('results', results)
-
-    setItens(results)
-
-  }
-
-  useEffect(function () {
-    carregarDados()
-  }
-  )
-
-
   return (
     <>
-      <div className="cards">
-        {/* Para cada um dos itens da lista, exibir um Card */}
-        {itens.map(function (elemento) {
-          return <Card item={elemento} key={elemento.name} />
-        })}
-      </div>
-
-
+      <Home/>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Card from '../components/Card'
+import { useEffect } from 'react'
+
+function Home() {
+
+  const [itens, setItens] = useState([])
+
+  async function carregarDados() {
+    const apiURL = "https://rickandmortyapi.com/api/character"
+
+    const response = await fetch(apiURL)
+
+    const body = await response.json()
+
+    const results = body.results
+
+    console.log('results', results)
+
+    setItens(results)
+
+  }
+
+  useEffect(function () {
+    carregarDados()
+  }
+  )
+
+
+  return (
+    <>
+      <div className="cards">
+        {/* Para cada um dos itens da lista, exibir um Card */}
+        {itens.map(function (elemento) {
+          return <Card item={elemento} key={elemento.name} />
+        })}
+      </div>
+
+
+    </>
+  )
+}
+
+export default Home
